0115d07ca855793b01eef71947bd0eb9c33d6bd0
Poncho <alfonsovaldes84@gmail.com>
2016-04-18 16:45:15 -0500

    add script

---
+++ script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+check=$(pgrep apache2)
+
+if ["$check" == ""]; then
+        apt install apache2 -y
+        /etc/init.d/apache2 start 2>1&
+fi
+
+sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/instant-search/g" /etc/apache2/sites-enabled/000-default.conf
+/etc/init.d/apache2 restart 2>1&
