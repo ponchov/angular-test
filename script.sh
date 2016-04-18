6728c2d4c58ab181d8475a7d8083e3e877d1c35f
Poncho <alfonsovaldes84@gmail.com>
2016-04-18 18:16:45 -0500

    Modify script

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 check="$(pgrep apache2| head -n1)"
-
+apacheconf="/etc/apache2/sites-enabled/000-default.conf"
 if ["$check" == ""]; then
         apt install apache2 -y
         /etc/init.d/apache2 start 2>1&
 fi
 
-sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/instant-search/g" /etc/apache2/sites-enabled/000-default.conf
+sed -i '/DocumentRoot \/var\/www\/html/d' $apacheconf || sed -i '/DocumentRoot \/var\/www\/html\/instant-search/d' $apacheconf
+sed -i '12 a DocumentRoot \/var\/www\/html\/instant-search' /etc/apache2/sites-enabled/000-default.conf
+
 /etc/init.d/apache2 restart 2>1&
